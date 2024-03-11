97c99d72cac9787885d5fdbdf23b9fe7b7cfd16b
Edsamn <eduardomsribas@gmail.comm>
2024-03-11 16:15:00 -0300

    criação de usuário

--- js/createUser.js
+++ js/createUser.js
@@ -5,17 +5,34 @@ const createUser = async (event) => {
     const name = document.getElementById("create-name-input").value;
     const email = document.getElementById("create-email-user-input").value;
     const pass = document.getElementById("create-password-input").value;
+    const error = document.getElementById("error");
 
     const user = {
       name: name,
       email: email,
       pass: pass,
     };
-    localStorage.setItem("users", JSON.stringify(user));
-    const usersSaved = JSON.parse(localStorage.getItem("users"));
+
+    const usersSaved = JSON.parse(localStorage.getItem("users")) || [];
+
+    const emailIndex = usersSaved.findIndex((user) => email === user.email);
+
+    if (emailIndex === -1) {
+      usersSaved.push(user);
+      localStorage.setItem("users", JSON.stringify(user));
+    } else {
+      error.innerHTML = `Email já cadastrado`;
+    }
+
     const response = await api.post("/createUser/crypto", usersSaved);
-    console.log(`Usuário criado com sucesso. ${response.data}`);
+
+    console.log(`Usuário criado com sucesso. ${response}`);
+
+    name.value = "";
+    email.value = "";
+    pass.value = "";
+    error.value = "";
   } catch (error) {
-    console.log(error.msg);
+    console.log(error);
   }
 };
